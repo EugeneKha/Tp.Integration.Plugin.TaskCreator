4bd13468bac085cf76316bc0c850b2b40bf51de3
Alla Pogotskaya <alla@targetprocess.com>
2011-05-10 12:43:55 +0300

    Initial Tasks Creation plugin commit

---
+++ Tp.Integration.Plugin.TaskCreator.Tests/CreateTasksForNewUserStoryHandlerSpecs.cs
@@ -0,0 +1,76 @@
+﻿// 
+// Copyright (c) 2005-2011 TargetProcess. All rights reserved.
+// TargetProcess proprietary/confidential. Use is subject to license terms. Redistribution of this file is strictly forbidden.
+// 
+using System.Linq;
+using NUnit.Framework;
+using Tp.Integration.Common;
+using Tp.Integration.Messages;
+using Tp.Integration.Messages.EntityLifecycle;
+using Tp.Integration.Messages.EntityLifecycle.Messages;
+using Tp.Integration.Plugin.Common.Storage.Persisters;
+using Tp.Integration.Testing.Common;
+using Tp.Testing.Common.NUnit;
+
+namespace Tp.Integration.Plugin.TaskCreator.Tests
+{
+	[TestFixture]
+	public class CreateTasksForNewUserStoryHandlerSpecs
+	{
+		private TaskCreatorProfile _profileInstance;
+		private TransportMock _transport;
+		private UserStoryCreatedMessage _userStoryCreatedMessage;
+
+		[SetUp]
+		public void Init()
+		{
+			_transport = TransportMock.Create(typeof (CreateTasksForNewUserStoryHandler).Assembly);
+
+			_profileInstance = CreateProfile("Profile#1");
+
+			_userStoryCreatedMessage = new UserStoryCreatedMessage
+			                           	{
+			                           		Dto =
+			                           			new UserStoryDTO
+			                           				{
+			                           					ProjectID = _profileInstance.Project,
+			                           					UserStoryID = 123,
+			                           					Name = "{CT}User Story Name"
+			                           				}
+			                           	};
+		}
+
+
+		[Test]
+		public void ShouldCreateTask()
+		{
+			_transport.HandleMessageFromTp(_userStoryCreatedMessage);
+
+			var taskDto = _transport.TpQueue.GetMessages<CreateCommand>().Last().Dto as TaskDTO;
+			taskDto.UserStoryID.Should(Be.EqualTo(_userStoryCreatedMessage.Dto.UserStoryID));
+			taskDto.Name.Should(Be.EqualTo(_profileInstance.TasksList));
+		}
+
+		#region Helpers
+
+		private static TaskCreatorProfile CreateProfile(string profileName)
+		{
+			var plugin = TransportMock.Plugin;
+
+			var account = new Account {Name = AccountName.Empty.Value};
+			plugin.Accounts.Add(account);
+
+			var profile = new Profile {Name = profileName, Initialized = true};
+			account.Profiles.Add(profile);
+
+			var profileInstance = profile.GetProfile<TaskCreatorProfile>();
+			profileInstance.Project = 123;
+			profileInstance.TasksList = "Task";
+			profileInstance.CommandName = "{CT}";
+			profile.SetProfile(profileInstance);
+			return profileInstance;
+		}
+
+		#endregion
+	}
+}
---
+++ Tp.Integration.Plugin.TaskCreator/TaskCreatorProfile.cs
@@ -0,0 +1,43 @@
+﻿// 
+// Copyright (c) 2005-2010 TargetProcess. All rights reserved.
+// TargetProcess proprietary/confidential. Use is subject to license terms. Redistribution of this file is strictly forbidden.
+// 
+using System;
+using Tp.Integration.Messages.ComponentModel;
+using Tp.Integration.Plugin.Common;
+
+namespace Tp.Integration.Plugin.TaskCreator
+{
+	[Profile, Serializable]
+	public class TaskCreatorProfile
+	{
+		public TaskCreatorProfile()
+		{
+			Project = int.MinValue;
+			TasksList = string.Empty;
+		}
+
+		[ProjectId]
+		[TPDescription("Select project (the plugin will work for user stories from this project)")]
+		[TpCategotyAttribute("Task Creator Settings")]
+		public int Project { get; set; }
+
+		private string _commandName = "{Type your command here}";
+
+		[TPDescription(@"As you add/edit a user story, you should start user story name with special command.
+e.g. we've chosen {CT} as a command in profile.<br/>
+<em>User Guide</em> is the name of this user story. So, if we put <em>{CT}User Guide</em> to user story name field, 
+a set of tasks specified below in 'Tasks List' field will be created<br/> and command <em>{CT}</em> will be cut from user story name.")]
+		[TpCategotyAttribute("Task Creator Settings")]
+		public string CommandName
+		{
+			get { return _commandName; }
+			set { _commandName = value; }
+		}
+
+		[TextArea]
+		[TPDescription("List of tasks to be created. One task per line.")]
+		[TpCategotyAttribute("Task Creator Settings")]
+		public string TasksList { get; set; }
+	}
+}
